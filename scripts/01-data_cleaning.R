5c76f2e8676de2af2410526ad9a7f6b3d67ef688
Mohid <mohidtheshariff27@gmail.com>
2022-04-27 04:17:27 +0000

    completed result graphs, working on model

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,35 +1,37 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Download data from Github
+# Author: Mohid Sharif 
+# Data: 26 April 2022
+# Contact: mohid.sharif@mail.utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+library (readr)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+### Data Download ###
+### From https://raw.githubusercontent.com/Hernan4444/MyAnimeList-Database/master/data/anime.csv
 
-#### What's next? ####
+# Datasets from MAL are located at "https://github.com/Hernan4444/MyAnimeList-Database/tree/master/data"
+# from which we require the dataset for all listed anime on MAL
+# we can do this by obtaining the url for the raw csv file that corresponds with our dataset
+# dataset link (see above)
+
+# Download data
+urlfile="https://raw.githubusercontent.com/Hernan4444/MyAnimeList-Database/master/data/anime.csv"
+
+mydata<-read_csv(url(urlfile))
+
+mydata$Score <- as.numeric(as.character(mydata$Score))
+mydata$Ranked <- as.numeric(as.character(mydata$Ranked))
+mydata$Episodes <- as.numeric(mydata$Episodes)
+
+
+### Save data ###
+write.csv(mydata, "inputs/data/anime_data.csv")
+
 
 
 
